833d4687cd0e2c1593b0bc6994bb9a48eaa411cc
Andrew Sergeev <sergeev.smartbox@gmail.com>
2020-05-05 15:23:39 +0300

    requested changes 2

--- UpSpace/UpSpace/Model/Response/LaunchList.swift
+++ UpSpace/UpSpace/Model/Response/LaunchList.swift
@@ -13,6 +13,6 @@ struct LaunchList: LaunchListProtocol, Decodable, Equatable {
     
     static func == (lhs: LaunchList, rhs: LaunchList) -> Bool {
         guard lhs.launches.count == rhs.launches.count else { return false }
-        return zip(lhs.launches, rhs.launches).allSatisfy { $0 == $1 }
+        return lhs.launches == rhs.launches
     }
 }
